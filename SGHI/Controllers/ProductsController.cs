eae38740fca6973c13e2062a549463fb4e6a1b9e
Madusalves <malvesduarte444@gmail.com>
2025-06-01 16:20:23 -0300

    Adicionar arquivos de projeto.

---
+++ SGHI/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SGHI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // This endpoint is protected by JWT authentication
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new[]
+            {
+                new { Id = 1, Name = "Laptop", Price = 1200 },
+                new { Id = 2, Name = "Phone", Price = 800 },
+                new { Id = 3, Name = "Tablet", Price = 600 }
+            };
+            return Ok(products);
+        }
+    }
+}
